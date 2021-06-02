1c12e90dc562bbf1f7d646284e4a34fe44b239d9
Martin Mertenat <41158878+Grolims@users.noreply.github.com>
2021-06-02 09:51:20 +0200

    test map

---
+++ resources/js/components/mapLeaflet.vue
@@ -0,0 +1,30 @@
+<template>
+  <l-map ref="map" style="height:300px" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+    <l-tile-layer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      layer-type="base"
+      name="OpenStreetMap"
+    ></l-tile-layer>
+    <l-feature-group>
+      <l-marker :lat-lng="[50, 50]"> </l-marker>
+    </l-feature-group>
+  </l-map>
+</template>
+<script>
+import { LMap, LTileLayer, LMarker, LFeatureGroup } from "@vue-leaflet/vue-leaflet";
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LFeatureGroup,
+  },
+  data() {
+    return {
+      zoom: 2,
+    };
+  },
+};
+</script>
+
+<style></style>
