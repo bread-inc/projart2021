db8fba26b86fd418e0d310175b001b7201310534
Mikael Ruffieux <mikael@redox-prod.ch>
2021-06-04 10:51:18 +0200

    Revert "Webdesign login"
    This reverts commit 6b4d57b006f3030b3b6ab070a75cfaddcfba7da4.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,72 +1,76 @@
 @extends('layouts.app')
 
 @section('content')
-
 <div class="container">
-    <div class="row mt-5">
-        <div class="col-12">
-            <div id="login-logo">
-                <img src="{{asset('storage/images/logo/logo.png')}}" alt="SwissGuesser">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+
+                    <a href="{{route('register')}}">Créer un compte</a>
+                    <a class="btn btn-secondary" href="{{route('home')}}">Continuer sans se connecter</a>
+                </div>
             </div>
         </div>
     </div>
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <div class="row my-5">
-            <div class="col-12">
-                <div class="form-group row">
-                    <h3 for="email" class="col-md-4 text-md-right">E-mail</h3>
-
-                    <div class="col-md-6">
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                </div>
-
-                <div class="form-group row">
-                    <h3 for="password" class="col-md-4 text-md-right">Mot de passe</h3>
-
-                    <div class="col-md-6">
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                </div>
-
-                @if (Route::has('password.request'))
-                    <a href="{{ route('password.request') }}">
-                        Mot de passe oublié ?
-                    </a>
-                @endif
-
-            </div>
-        </div>
-        
-        <div class="row">
-            <div class="col-12">
-
-                <button type="submit" class="btn btn-gradient">
-                    Valider
-                </button>
-    
-                <a href="{{route('register')}}" class="d-block text-center my-3">Créer un compte</a>
-                <a href="{{route('home')}}" class="d-block text-center my-3">Continuer sans se connecter</a>
-                
-            </div>
-        </div>
-    </form>
-
 </div>
-
 @endsection
