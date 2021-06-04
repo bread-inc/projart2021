358eaeec1f0232574222cbcc06084cf09f3bbb1d
Mikael Ruffieux <mikael@redox-prod.ch>
2021-06-04 10:47:50 +0200

    Conflicts merge

--- resources/js/components/mapLeaflet.vue
+++ resources/js/components/mapLeaflet.vue
@@ -1,8 +1,7 @@
 <template>
   <button class="btn btn-info" @click="getUserPosition">locate</button>
   <button class="btn btn-warning" @click="creaIndice"> indice</button>
-  <button class="btn btn-warning" @click="checkDistance"> find </button>
-   <button class="btn btn-warning" @click="locateEvry"> localisationauto </button>
+  <button class="btn btn-warning" @click="findDistance"> find </button>
 <div id="game-map">
   <l-map
     ref="map"
@@ -12,20 +11,23 @@
       userLocation.lat || defaultLocation.lat,
       userLocation.lng || defaultLocation.lng
     ]"
-
+    @update:center="centerUpdated"
   >
     <l-tile-layer
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       layer-type="base"
       name="OpenStreetMap"
     ></l-tile-layer>
- <l-marker :lat-lng="[userLocation.lat || defaultLocation.lat,userLocation.lng || defaultLocation.lng]">
+ <l-marker :lat-lng="[userLocation.lat || defaultLocation.lat,
+      userLocation.lng || defaultLocation.lng]">
        <l-popup>
          You are here
         </l-popup>
-<l-circle :lat-lng="[userLocation.lat || 0, userLocation.lng || 0]" :radius="10" color="blue" />
 </l-marker>
 <l-marker :lat-lng="[questionLocation.lat,questionLocation.lng]">
+       <l-popup>
+         You are here
+        </l-popup>
 </l-marker>
 <l-circle :lat-lng="[indiceCircle.lat || 0,indiceCircle.lng || 0]" :radius="indiceCircle.radius || 0" color="red" />
   </l-map>
@@ -49,12 +51,15 @@ export default {
       default: () => ({
         lat: 46.783823675203024,
         lng: 6.645928812723402,
-        radius: 300,
+        radius: 50,
       })
     },
   },
   computed:{
-
+      returnPosUser()
+      {
+          return this.userLocation.lng;
+      }
   },
   data() {
     return {
@@ -62,8 +67,6 @@ export default {
       indiceCircle: {},
       distanceQuestionPosuser:0,
       zoom: 18,
-      distanceOk: false,
-
     };
   },
   init(){
@@ -96,53 +99,27 @@ export default {
         this.indiceCircle = {
         lat: 46.783823675203024,
         lng: 6.645928812723402,
-        radius: 350, //premier 750
+        radius: 50,
         }
     },
 
     findDistance() {
     let distance = this.mapleaf.distance([this.userLocation.lat|| 0, this.userLocation.lng || 0] ,[this.questionLocation.lat, this.questionLocation.lng]);
     this.distanceQuestionPosuser = distance;
+    console.log(distance);
+   // console.log(this.distanceQuestionPosuser);
     },
 
-    //store object map from leaflet
     storemap(mapObject) {
     this.mapleaf = mapObject
     },
 
-    checkDistance()
-    {
-        this.findDistance();
-        console.log(this.distanceQuestionPosuser);
-        if(this.questionLocation.radius < this.distanceQuestionPosuser )
-        {
 
-            console.log("ouvre page réssayer");
-            this.distanceOK = false;
-
-        }else{
-        this.distanceOK = true;
-        console.log("ouvre page okaywithscore");
-        }
-
+    centerUpdated(center){
+        this.center = center;
+        //console.log(this.center = center);
 
     },
-
-    locateEvry()
-    {
-        let interval = window.setInterval(function()
-        {
-            this.getUserPosition();
-        }, 500);
-
-        interval;
-    },
-
-    findRandomPosition()
-    {
-
-    },
-
 
   }
 };
