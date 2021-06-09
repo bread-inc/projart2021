6ae5509e04844b8c631b0bdd0b63fdc02eaaf67d
Martin Mertenat <41158878+Grolims@users.noreply.github.com>
2021-06-09 14:24:04 +0200

    map desktop

--- resources/views/public/components/desktop-menu.blade.php
+++ resources/views/public/components/desktop-menu.blade.php
@@ -9,7 +9,7 @@
         <h3>Les quizzes</h3>
         <ul>
             <li><a href="{{route('region.index')}}"><i class="far fa-mountains mr-2"></i>Les régions</a></li>
-            <li><a href="#"><i class="far fa-map mr-2"></i>La carte</a></li>
+            <li><a href="{{route('map.desktop')}}"><i class="far fa-map mr-2"></i>La carte</a></li>
             <li><a href="{{route('global-ranking')}}"><i class="fas fa-chart-bar mr-2"></i>Classement général</a></li>
         </ul>
     </div>
@@ -23,7 +23,7 @@
             <li><a href="{{route('profile.show', [auth()->id(), '#scores'])}}"><i class="fas fa-history mr-2"></i>Mon historique</a></li>
             @if (auth()->user()->isAdmin)
             <li><a href="{{route('admin.dashboard')}}"><i class="far fa-cog mr-2"></i>Administration</a></li>
-                
+
             @endif
         </ul>
     </div>
