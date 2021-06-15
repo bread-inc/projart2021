b4528280e8e98c1906722fa2c1bd8bf5f548df5f
Mikkel Vestergaard <56039404+vestermikk@users.noreply.github.com>
2021-06-15 17:24:06 +0200

    Added scoreboardTop function to Scoreboard Trait

--- app/Http/Traits/ScoreboardTrait.php
+++ app/Http/Traits/ScoreboardTrait.php
@@ -13,7 +13,7 @@ trait ScoreboardTrait
     /**
      * Returns the global rankings data in JSON.
      * 
-     * @return string|false The JSON encoded global scoreboard, or false if empty
+     * @return string|false JSON encoded global scoreboard, false if empty
      */
     public function scoreboard() {
         $scores = Score::groupBy('user_id')
@@ -38,6 +38,41 @@ trait ScoreboardTrait
         {
             $rankings[$i]["rank"] = $i+1;
         }
+
+        return json_encode($rankings);
+    }
+
+    /**
+     * Returns given integer of global rankings starting from the top
+     * 
+     * @param int Number of scores from the top
+     * @return string|false JSON encoded scoreboard, false if empty
+     */
+    public function scoreboardTop(int $n) {
+        $scores = Score::groupBy('user_id')
+            ->selectRaw('sum(score) as total, user_id')
+            ->pluck('total', 'user_id');
+
+        $rankings = [];
+        $i = 0;
+
+        foreach ($scores as $score) {
+            $user_id = $i + 1; 
+            $pseudo = User::findOrFail($user_id)->pseudo;
+            $avatar = User::findOrFail($user_id)->avatar;
+            array_push($rankings, ["user_id" => $user_id, "pseudo" => $pseudo, "avatar" => $avatar, "score" => $score]);
+            $i++;
+        }
+
+        $score = array_column($rankings, 'score');
+        array_multisort($score, SORT_DESC, $rankings);
+
+        for ($i=0; $i < sizeof($rankings); $i++) 
+        {
+            $rankings[$i]["rank"] = $i+1;
+        }
+
+        $rankings = array_slice($rankings, 0, $n);
 
         return json_encode($rankings);
     }
