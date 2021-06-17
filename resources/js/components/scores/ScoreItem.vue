cc58a610ce9b58e9895f419ccf40a2b90e0ac066
Mikkel Vestergaard <vestergaard.mikk@gmail.com>
2021-06-17 10:04:10 +0200

    Updated ScoreItem URLs

--- resources/js/components/scores/ScoreItem.vue
+++ resources/js/components/scores/ScoreItem.vue
@@ -3,7 +3,7 @@
       <td scope="row">{{ score.rank }}</td>
       <td><img :src="score.avatar" :alt="'alt text'" class="rounded-circle" /></td>
       <td>
-        <a :href="'/user/' + score.user_id"
+        <a :href="'/bread/user/' + score.user_id"
           ><b>{{ score.pseudo }}</b></a
         >
         <small>{{ score.score }} pts</small>
