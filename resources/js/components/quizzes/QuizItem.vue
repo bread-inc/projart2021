0deb7377295e5bf7d86fc4ad932bacb6f4dc00a0
Kevin Oliveira Paiva <kevin.paivaoliveira@heig-vd.ch>
2021-06-17 15:42:43 +0200

    Fix css quiz desktop map

--- resources/js/components/quizzes/QuizItem.vue
+++ resources/js/components/quizzes/QuizItem.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- element-->
   <a :href="'/bread/quiz/' + quiz.id + '/info'" class="quiz quiz-3x">
-      <div v-if="quiz.picture" class="quiz-thumb" :style="'background-image: url(\&#39;' + quiz.picture +'\&#39;);'"></div>
+      <div v-if="quiz.picture" class="quiz-thumb quizMap" :style="'background-image: url(\&#39;' + quiz.picture +'\&#39;);'"></div>
       <div v-else class="quiz-thumb">
           <h5>{{quiz.title}}</h5>
       </div>
